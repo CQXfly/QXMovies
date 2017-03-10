093dbbc2021d1d877f7fefe0ea3cff4a6c138adf
chongqingxu <firefoxdance@163.com>
2017-03-10 09:52:39 +0800

    add cors module

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use('/fly',fly);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var cors = require('cors');
 
 var mongoose = require('mongoose');
 var Movies = require('../model/movie').Movies;
@@ -8,17 +9,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/movies',function (req,res,next) {
+router.get('/movies',cors(),function (req,res,next) {
     var size = req.query.size ;
     if (size == undefined){size = 10};
     var index = req.query.index;
     if (index == undefined){index = 0};
-
+    // res.header("Access-Control-Allow-Origin","*");
     // 查询十个数据
     Movies.getMoviesByIndexandSize(index,size,function (docs) {
 
-        res.append('Access-Control-Allow-Origin','*');
-        res.send(docs);
+        // res.append('Access-Control-Allow-Origin','*');
+        res.send(JSON.stringify(docs));
     })
 
 });
